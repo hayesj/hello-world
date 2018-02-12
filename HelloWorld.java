f410778648759c26eb0d9aacb1ecc7324d59c815
Jane Hayes <jane.e.hayes@gmail.com>
2018-02-12 14:15:33 -0500

    changed to have string say

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,8 +5,13 @@ import java.util.*;
  */
 public class HelloWorld {
     
-    public static void main (String[] args) {
-	System.out.println ("Hello world!");
+  //  public static void main (String[] args) {
+//	System.out.println ("Hello world!");
+
+	public String say() {
+		  return("Hello World!");
+	}
+	
 	System.out.println("Hello change");
     }
 }
