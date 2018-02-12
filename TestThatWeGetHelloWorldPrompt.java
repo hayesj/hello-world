64e540b05281ba89e3d1de00e3d6850a8443e335
Jane Hayes <jane.e.hayes@gmail.com>
2018-02-12 11:20:14 -0500

    this adds a Junit test for hello world

---
+++ TestThatWeGetHelloWorldPrompt.java
@@ -0,0 +1,17 @@
+import junit.framework.TestCase;
+
+public class TestThatWeGetHelloWorldPrompt
+    extends TestCase {
+    public TestThatWeGetHelloWorldPrompt(
+        String name) {
+        super(name);
+    }
+    public void testSay() {
+        HelloWorld hi = new HelloWorld();
+        assertEquals("Hello World!", hi.say());
+    }
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(
+            TestThatWeGetHelloWorldPrompt.class);
+    }
+}
