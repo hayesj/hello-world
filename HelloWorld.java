275e2fa5d29ccb78a84598de66c61178a3fec66a
Jane Hayes <jane.e.hayes@gmail.com>
2018-02-13 10:27:08 -0500

    changed file to have calculator in it

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,9 +5,16 @@ import java.util.*;
  */
 public class HelloWorld {
     
-  public static void main (String[] args) {
+  //public static void main (String[] args) {
 
-    System.out.println ("! This is a simple two lines");
-    System.out.println("Hello world!!");
+  //  System.out.println ("! This is a simple two lines");
+  //  System.out.println("Hello world!!");
+   // }
+
+    public int evaluate(String expression)  {
+         int sum = 0;
+         for (String summand: expression.split("\\+"))
+            sum += Integer.valueOf(summand);
+        return sum;
     }
 }
