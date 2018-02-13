0c1dd7abafec7bbbd9e262715bf299854ea903bb
Jane Hayes <jane.e.hayes@gmail.com>
2018-02-13 10:27:36 -0500

    changed file to hav calculator test

--- HelloWorldTest.java
+++ HelloWorldTest.java
@@ -6,16 +6,22 @@ import static org.junit.Assert.*;
 public class HelloWorldTest {
 
   @Test
+  public void evaluateExpression() { 
 
-    public void testMain() { 
 
-      String[] expected = new String[] {
-        "Yeah! This is a simple", "Hello World!!" };
-      String[] results =
-          AbstractMainTests.executeMain("HelloWorld", new String[] {"Yeah"});
+      HelloWorld hello = new HelloWorld();
+      int sum = hello.evaluate("1+2+3");
+      assertEquals(6, sum);
 
-          assertArrayEquals(expected, results);
-            }
+    }
+
+     // String[] expected = new String[] {
+     //   "Yeah! This is a simple", "Hello World!!" };
+     // String[] results =
+     //     AbstractMainTests.executeMain("HelloWorld", new String[] {"Yeah"});
+
+      //    assertArrayEquals(expected, results);
+       //     }
 
           }
 
